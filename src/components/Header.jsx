036829011039782bbf3582c2ece308e94a37ee89
kilik42 <marvinevins@yahoo.com>
2023-02-26 00:18:11 +0000

    added header section complete with css files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './Header.css';
-const  Header=() => {
+
+function Header() {
   return (
-<div className="header">
+    <div className="header">
       <div className="header__logo">
         <img src="amazon-logo.png" alt="Amazon Logo" />
       </div>
@@ -13,6 +14,14 @@ const  Header=() => {
           <li><a href="#">About</a></li>
           <li><a href="#">Contact</a></li>
         </ul>
+      </div>
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <button className="header__searchButton">Search</button>
+      </div>
+      <div className="header__options">
+        <span className="header__optionLineOne">Hello, Guest</span>
+        <span className="header__optionLineTwo">Sign In</span>
       </div>
     </div>
   );
